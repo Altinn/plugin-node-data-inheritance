0cbc2272b37d8f0ddc20c992cee87dc15030d58c
David Emanuelsen <david.emanuelsen@outlook.com>
2016-10-12 12:55:17 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,12 @@
+# Data Inheritance Plugin for Pattern Lab Node
+
+The Data Inheritance Plugin allows patterns to inherit data from patterns within its lineage. 
+This means that data from included patterns is merged with the current pattern. The current pattern's data takes precedence.
+
+## Installation
+
+To add the Custom Output Plugin to your project using [npm](http://npmjs.com/):
+
+    npm install git+https://github.com/Altinn/plugin-node-data-inheritance.git
+
+Or add it directly to your project's `package.json` file and run `npm install`.
---
+++ index.js
@@ -0,0 +1,132 @@
+var pluginName = 'plugin-node-data-inheritance'; 
+var path = require('path');
+var fs = require('fs-extra'); 
+var glob = require('glob');
+
+function getPatternByName (patternlab, patternName) {
+  for (var i = 0; i < patternlab.patterns.length; i++) {
+    if (patternlab.patterns[i].name === patternName) {
+      return patternlab.patterns[i];
+    }
+  }
+
+  return null;
+}
+
+function arrayReplaceRecursive (arr) {
+  var i = 0
+  var p = ''
+  var argl = arguments.length
+  var retObj
+
+  if (argl < 2) {
+    throw new Error('There should be at least 2 arguments passed to arrayReplaceRecursive()')
+  }
+
+  if (Object.prototype.toString.call(arr) === '[object Array]') {
+    retObj = []
+    for (p in arr) {
+      retObj.push(arr[p])
+    }
+  } else {
+    retObj = {}
+    for (p in arr) {
+      retObj[p] = arr[p]
+    }
+  }
+
+  for (i = 1; i < argl; i++) {
+    for (p in arguments[i]) {
+      if (retObj[p] && typeof retObj[p] === 'object') {
+        retObj[p] = arrayReplaceRecursive(retObj[p], arguments[i][p])
+      } else {
+        retObj[p] = arguments[i][p]
+      }
+    }
+  }
+  return retObj
+}
+
+function onPatternIterate (patternlab, pattern) {
+  if (pattern.patternLineages) {
+    for (var i = 0; i < pattern.patternLineages.length; i++) {
+      var lineagePathParts = pattern.patternLineages[i].lineagePath.split('\\');
+      var lastPartParts = lineagePathParts[lineagePathParts.length - 1].split('.');
+
+      var currentPattern = getPatternByName(patternlab, lastPartParts[0]);
+      if (currentPattern) {
+        onPatternIterate(patternlab, currentPattern);
+        if (!pattern.jsonFileData) {
+          pattern.jsonFileData = currentPattern.jsonFileData;
+        } else {
+          pattern.jsonFileData = arrayReplaceRecursive(pattern.jsonFileData, currentPattern.jsonFileData);
+        }
+      }
+    }
+  }
+}
+
+function registerEvents (patternlab) {
+  patternlab.events.on('patternlab-pattern-before-data-merge', onPatternIterate);
+}
+
+function getPluginFrontendConfig () {
+  return {
+    'name': 'pattern-lab\/' + pluginName, 'templates': [],
+    'stylesheets': [],
+    'javascripts': ['patternlab-components\/pattern-lab\/' + pluginName +
+      '\/js\/' + pluginName + '.js'],
+    'onready': 'PluginDataInheritance.init()', 'callback': ''
+  }
+}
+
+function pluginInit (patternlab) {
+  if (!patternlab) {
+    console.error('patternlab object not provided to plugin-init');
+    process.exit(1);
+  }
+
+  var pluginConfig = getPluginFrontendConfig();
+  var pluginConfigPathName = path.resolve(patternlab.config.paths.public.root,
+    'patternlab-components', 'packages');
+
+  try {
+    fs.outputFileSync(pluginConfigPathName + '/' + pluginName + '.json',
+      JSON.stringify(pluginConfig, null, 2));
+  } catch (ex) {
+    console.trace(
+      'plugin-node-tab: Error occurred while writing pluginFile configuration');
+    console.log(ex);
+  }
+
+  if (!patternlab.plugins) { 
+    patternlab.plugins = [] 
+  }
+
+  patternlab.plugins.push(pluginConfig);
+  var pluginFiles = glob.sync(__dirname + '/dist/**/*');
+  if (pluginFiles && pluginFiles.length > 0) {
+   
+    for (var i = 0; i < pluginFiles.length; i++) {
+      try {
+        var fileStat = fs.statSync(pluginFiles[i]);
+        if (fileStat.isFile()) {
+          var relativePath = path.relative(__dirname, pluginFiles[i]).replace('dist', '');
+          var writePath = path.join(patternlab.config.paths.public.root,
+            'patternlab-components', 'pattern-lab', pluginName, relativePath);
+          var tabJSFileContents = fs.readFileSync(pluginFiles[i], 'utf8');
+          fs.outputFileSync(writePath, tabJSFileContents);
+        }
+      } catch (ex) {
+        console.trace(
+          'plugin-node-tab: Error occurred while copying pluginFile',
+          pluginFiles[i]);
+        console.log(ex);
+      }
+    }
+  }
+
+  registerEvents(patternlab); patternlab.config[pluginName] = true;
+}
+
+module.exports = pluginInit
